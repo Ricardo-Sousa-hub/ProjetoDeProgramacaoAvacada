e57aee06cb2a1eec8f7930cd1ac792304cc86611
Ricardo Sousa <satao.net@hotmail.com>
2022-05-27 12:18:29 +0100

    Chamada das funções de criação das tabelas Criação das classes/tabelas TabelaBDModelos e TabelaBDTipoCombustivel, TabelaBDModelos e TabelaBDTipoDespesa.

--- app/src/main/java/pt/ipg/projetofinal/BDAppOpenHelper.kt
+++ app/src/main/java/pt/ipg/projetofinal/BDAppOpenHelper.kt
@@ -12,7 +12,10 @@ class BDAppOpenHelper(
         requireNotNull(db)// if(db == null) return
 
         TabelaBDUtilizadores(db).cria()
+        TabelaBDTipoCombustivel(db).cria()
+        TabelaBDModelos(db).cria()
         TabelaBDCarros(db).cria()
+        TabelaBDTipoDespesa(db).cria()
         TabelaBDDespesas(db).cria()
     }
 
