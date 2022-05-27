03d31c0c06cf383ca6d5305827d9af3d4faadab1
Ricardo Sousa <satao.net@hotmail.com>
2022-05-27 12:09:26 +0100

    Alterações necessárias nas classes Carro e Despesa.

--- app/src/main/java/pt/ipg/projetofinal/Carro.kt
+++ app/src/main/java/pt/ipg/projetofinal/Carro.kt
@@ -5,8 +5,8 @@ import android.content.ContentValues
 data class Carro(
     var id: Long,
     var data_introduzida: String,
-    var tipo_combustivel: String,
-    var modelo: String,
+    var id_tipo_combustivel: Long,
+    var id_modelo: Long,
     var id_utilizador: Long
     ) {
 
@@ -14,8 +14,8 @@ data class Carro(
         val valores = ContentValues()
 
         valores.put(TabelaBDCarros.DATA_INSERT, data_introduzida)
-        valores.put(TabelaBDCarros.TIPO_COMBUSTIVEL, tipo_combustivel)
-        valores.put(TabelaBDCarros.MODELO, modelo)
+        valores.put(TabelaBDCarros.ID_TIPO_COMBUSTIVEL, id_tipo_combustivel)
+        valores.put(TabelaBDCarros.ID_MODELO, id_modelo)
         valores.put(TabelaBDCarros.ID_UTILIZADOR, id_utilizador)
 
         return valores
--- app/src/main/java/pt/ipg/projetofinal/Despesa.kt
+++ app/src/main/java/pt/ipg/projetofinal/Despesa.kt
@@ -4,7 +4,7 @@ import android.content.ContentValues
 
 data class Despesa(
     var id: Long,
-    var tipo_despesa: String,
+    var id_tipo_despesa: Long,
     var data_despesa: String,
     var valor_despesa: Float,
     var id_carro: Long
@@ -13,7 +13,7 @@ data class Despesa(
     fun toContentValues() : ContentValues{
         val valores = ContentValues()
 
-        valores.put(TabelaBDDespesas.TIPO_DESPESA, tipo_despesa)
+        valores.put(TabelaBDDespesas.ID_TIPO_DESPESA, id_tipo_despesa)
         valores.put(TabelaBDDespesas.DATA_DESPESA, data_despesa)
         valores.put(TabelaBDDespesas.VALOR_DESPESA, valor_despesa)
         valores.put(TabelaBDDespesas.ID_CARRO, id_carro)
