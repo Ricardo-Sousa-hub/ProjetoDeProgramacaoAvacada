955e9a725325364898c642bf47faf8adf2a6aa69
Ricardo <satao.net@hotmail.com>
2022-06-16 23:34:52 +0100

    Criação do fragment para a pagina dos utilizadores

--- app/src/main/java/pt/ipg/projetofinal/ui/utilizadores/UtilizadoresFragment.kt
+++ app/src/main/java/pt/ipg/projetofinal/ui/utilizadores/UtilizadoresFragment.kt
@@ -1,4 +1,4 @@
-package pt.ipg.projetofinal.ui.home
+package pt.ipg.projetofinal.ui.utilizadores
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import pt.ipg.projetofinal.databinding.FragmentHomeBinding
+import pt.ipg.projetofinal.databinding.FragmentUtilizadoresBinding
 
-class HomeFragment : Fragment() {
+class UtilizadoresFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentUtilizadoresBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val utilizadoresViewModel =
+            ViewModelProvider(this).get(UtilizadoresViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentUtilizadoresBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        utilizadoresViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/pt/ipg/projetofinal/ui/utilizadores/UtilizadoresViewModel.kt
+++ app/src/main/java/pt/ipg/projetofinal/ui/utilizadores/UtilizadoresViewModel.kt
@@ -1,13 +1,13 @@
-package pt.ipg.projetofinal.ui.home
+package pt.ipg.projetofinal.ui.utilizadores
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class UtilizadoresViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is utilizadores Fragment"
     }
     val text: LiveData<String> = _text
 }
